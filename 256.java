9991cb30298bb923a6745fe4630e8108f38b4fcb
YuriyVMalik <YuriyVMalik@gmail.com>
2021-11-29 21:46:46 +0200

    AddGit

--- 256.java
+++ 256.java
@@ -6,10 +6,42 @@ Test 4 23-30
 Test 4 23-35
 Test 4 14-57 - 21.11.2021
 Test 4 16-06 - 21.11.2021
-<<<<<<< HEAD
 Test 4 18-16 - 21.11.2021
-=======
-Test 4 18-21 - 21.11.2021
->>>>>>> newVetka
 ыы vfvfdvdfvdf
 user2
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
+User2 (addGit)
